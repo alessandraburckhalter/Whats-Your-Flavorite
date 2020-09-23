685e2cf39d68bca09af9a1c5bc7cc0bb403ccd9f
Heeyoung Song <hsong8179@gmail.com>
2020-09-23 10:25:36 -0400

    added bread and nuts

---
+++ index.js
@@ -0,0 +1,4 @@
+const bread= ["Basmati Rice", "Bowtie Pasta", "Bread", "Brown Rice", "Corn Tortillas", "Digestive Biscuits", "Flour Tortilla", "Jasmine Rice", "Rice", "Rice Noodles", "Rice Stick Noodles", "Rice Vermicelli", "Rigatoni", "Spaghetti", "Wholegrain Bread", "Vermicelli Pasta", "Pappardelle Pasta", "Paccheri Pasta", "Linguine Pasta", "Crusty Bread", "Tortillas", "Udon Noodles", "Noodles", "English Muffins", "Muffins", "Rice Krispies", "Bun", "Potatoe Buns", "Pita Bread", "Sesame Seed Burger Buns", "Buns", ""]
+
+const nuts = ["Cashew Nuts", "Cashews", "Chickpeas", "Coriander Seeds", "Cumin Seeds", "Fennel Seeds", "Flaked Almonds", "Mustard Seeds", "Peanuts", "Peas", "Pine Nuts", "Sesame Seed", "Sugar Snap Peas",  "Walnuts", "Pecan Nuts", "Chestnuts", "Almonds", "Oats"]
+
